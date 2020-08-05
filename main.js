c33b410af24f0ce5878461572c27d0f0e2ebc5fb
rosscodingrockstars <hey@jenniferross.tech>
2020-08-05 15:58:25 -0400

    experimented with SNOBN'US

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-alert("this is a test")
+// const numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for (let i = 0; i < numberArray.length; i++) {
+//   console.log(numberArray[i]);
+// }
+const favoriteFoods = {
+  breakfast: "bacon & eggs",
+  lunch: "cheddar bowls",
+  dinner: "pizza",
+  dessert: "brownies",
+};
+
+console.log(favoriteFoods.dinner);
