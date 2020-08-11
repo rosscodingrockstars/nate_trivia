1902ec366f188f31b84851fbee44579b73f6b997
rosscodingrockstars <hey@jenniferross.tech>
2020-08-11 16:01:14 -0400

    added psuedocode

--- main.js
+++ main.js
@@ -10,38 +10,44 @@ const favoriteFoods = {
 };
 
 console.log(favoriteFoods.dinner);
-const breakfastCard = `
-<div class="card" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">${favoriteFoods.breakfast}</h5>
-    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="card-link">Card link</a>
-    <a href="#" class="card-link">Another link</a>
-  </div>
-</div>
-`;
+// const breakfastCard = `
+// <div class="card" style="width: 18rem;">
+//   <div class="card-body">
+//     <h5 class="card-title">${characters[i].name</h5>
+//     <h6 class="card-subtitle mb-2 text-muted">${characters[i].location//</h6>
+//     <p class="card-text">I love ${characters[i].loves</p>
+//     <a href="#" class="card-link">Card link</a>
+//     <a href="#" class="card-link">Another link</a>
+//   </div>
+// </div>
+// `;
 const characters = [
   {
     name: "Leslie Knope",
     location: "Pawnee, India",
-    loves: "waffles",
-
+    loves: "waffles"
+  },
+  {
     name: "Ron Swanson",
     location: "Pawnee, India",
-    loves: "silence",
-
+    loves: "silence"
+  },
+  {
     name: "April Ludgate",
     location: "Pawnee, India",
-    loves: "Halloween",
-
+    loves: "Halloween"
+  },
+  {
     name: "Tom Haverford",
     location: "Pawnee, India",
     loves: "coconut water"
   }
 ];
 for (let i = 0; i < characters.length; i++) {
-  console.log(characters[i].name);
+  const characterCard = `
+<div class="card" style="width: 18rem;"><div class="card-body"><h5 class="card-title">${characters[i].name}</h5><h6 class="card-subtitle mb-2 text-muted">${characters[i].location}</h6><p class="card-text">I love ${characters[i].loves}.</p></div></div> 
+ `;
+  $(".col").append(characterCard);
 }
 // $(".col").append(breakfastCard);
 // console.log(favoriteFood.breakfast);
