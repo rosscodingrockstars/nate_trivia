9fb2be19312c9924b22959a400202bdad54eceda
jenross <ross.m.jennifer@gmail.com>
2020-12-05 11:37:23 -0500

    clean up

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const watermelon = [
     question: "The first recorded watermelon harvest was in which place?",
     answerChoices: ["China", "Cuba", "Egypt", "Poland"],
     answer: "Egypt",
-    gif: "images/gif1.gif"
+    gif: "images/gif1.gif",
   },
   {
     question:
@@ -12,34 +12,34 @@ const watermelon = [
       "26.50 pounds",
       "350.50 pounds",
       "30.00 pounds",
-      "478.90 pounds"
+      "478.90 pounds",
     ],
     answer: "350.50 pounds",
-    gif: "images/gif2.gif"
+    gif: "images/gif2.gif",
   },
   {
     question: "About what percentage of watermelon is water?",
     answerChoices: ["92%", "91%", "93%", "97%"],
     answer: "92%",
-    gif: "images/gif3.gif"
+    gif: "images/gif3.gif",
   },
   {
     question: "Which vitamin type Isn't In watermelon?",
     answerChoices: ["A", "C", "B", "D"],
     answer: "D",
-    gif: "images/gif4.gif"
+    gif: "images/gif4.gif",
   },
   {
     question: "About how many calories per 100 grams in watermelon is there?",
     answerChoices: ["60", "30", "20", "90"],
     answer: "30",
-    gif: "images/gif5.gif"
+    gif: "images/gif5.gif",
   },
   {
     question: "Is watermelon a fruit?",
     answerChoices: ["Yes", "No"],
     answer: "Yes",
-    gif: "images/gif6.gif"
+    gif: "images/gif6.gif",
   },
   {
     question: "Which temperature range should your watermelon be grown in?",
@@ -47,66 +47,59 @@ const watermelon = [
       "70 to 90 degrees",
       "40 to 60 degrees",
       "100 to 120 degrees",
-      "-70 to 5 degrees"
+      "-70 to 5 degrees",
     ],
     answer: "70 to 90 degrees",
-    gif: "images/gif7.gif"
+    gif: "images/gif7.gif",
   },
   {
     question: "Is watermelon healthy?",
     answerChoices: ["Yes", "No"],
     answer: "Yes",
-    gif: "images/gif8.webp"
+    gif: "images/gif8.webp",
   },
   {
     question:
       "Per 100 grams of watermelon, about how many grams of sugar Is there?",
     answerChoices: ["0 grams", "2 grams", "6 grams", "10 grams"],
     answer: "6 grams",
-    gif: "images/gif9.webp"
+    gif: "images/gif9.webp",
   },
   {
     question: "What percentage of watermelon is water?",
     answerChoices: ["90%", "93%", "60%", "92%"],
     answer: "92%",
-    gif: "images/gif10.webp"
-  }
+    gif: "images/gif10.webp",
+  },
 ];
-let questionCount = 0;
-console.log(watermelon.length);
+
+let questionCount;
+
 function handleQandA() {
   if (questionCount < watermelon.length) {
     const Question = `<form>
   <h2>${watermelon[questionCount].question}</h2>
                          
   <div class="form-check">
-    <input class="form-check-input" type="radio" name="radioButtons" id="radios1" value="${
-      watermelon[questionCount].answerChoices[0]
-    }" checked>
+    <input class="form-check-input" type="radio" name="radioButtons" id="radios1" value="${watermelon[questionCount].answerChoices[0]}" checked>
     <label class="form-check-label" for="radios1">
     ${watermelon[questionCount].answerChoices[0]}
     </label>
   </div>
   <div class="form-check">
-    <input class="form-check-input" type="radio" name="radioButtons" id="radios2" value="${
-      watermelon[questionCount].answerChoices[1]
-    }">
+    <input class="form-check-input" type="radio" name="radioButtons" id="radios2" value="${watermelon[questionCount].answerChoices[1]}">
     <label class="form-check-label" for="radios2">
     ${watermelon[questionCount].answerChoices[1]}
     </label>
   </div>
   <div class="form-check">
-    <input class="form-check-input" type="radio" name="radioButtons" id="radios3" value="${
-      watermelon[questionCount].answerChoices[2]
-    }">
+    <input class="form-check-input" type="radio" name="radioButtons" id="radios3" value="${watermelon[questionCount].answerChoices[2]}">
     <label class="form-c-label" for="radios3">
     ${watermelon[questionCount].answerChoices[2]}
     </label>
   </div>
   <div class="form-check">
-    <input class="form-check-input" type="radio" name="radioButtons" id="radios4" value="${
-      watermelon[questionCount].answerChoices[3]
-    }">
+    <input class="form-check-input" type="radio" name="radioButtons" id="radios4" value="${watermelon[questionCount].answerChoices[3]}">
     <label class="form-check-label" for="radios4">
     ${watermelon[questionCount].answerChoices[3]}
       </label>
@@ -119,11 +112,11 @@ function handleQandA() {
     displayResults();
   }
 }
-console.log($("input:checked").val());
-const answer = `${watermelon[questionCount].answer}`;
+
 function handleSubmit() {
-  $(".submit").click(function(e) {
+  $(".submit").click(function (e) {
     e.preventDefault();
+    let answer = `${watermelon[questionCount].answer}`;
     if (answer === $("input:checked").val()) {
       correct();
     } else {
@@ -132,13 +125,20 @@ function handleSubmit() {
     handleNext();
   });
 }
+
 function handleNext() {
-  $(".next").click(function(e) {
+  $(".next").click(function (e) {
     e.preventDefault();
-    questionCount++;
+    $(".answer-check").text("");
+    $(".gif").attr("src", "");
+    $(".gif").hide();
+    $(".next").hide();
+    $("form").text("");
     $("form").show();
+    handleQandA();
   });
 }
+
 function incorrect() {
   $("form").hide();
   $(".answer-check").text("incorrect");
@@ -147,6 +147,7 @@ function incorrect() {
   $(".gif-container").append(
     $(".gif").attr("src", watermelon[questionCount].gif)
   );
+  questionCount++;
 }
 
 function correct() {
@@ -157,13 +158,17 @@ function correct() {
   $(".gif-container").append(
     $(".gif").attr("src", watermelon[questionCount].gif)
   );
+  questionCount++;
 }
+
 function displayResults() {
   console.log("results");
 }
+
 function startQuiz() {
   $(".gif").hide();
-$(".next").hide();
+  $(".next").hide();
   handleQandA();
 }
+
 startQuiz();
